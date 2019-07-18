37b154f28619f96cf76411563d86a34b12ef2582
Abdullah Abbasi <abdullah.abbasi@oath.com>
2019-07-17 23:58:26 -0700

    fix key

--- routes.js
+++ routes.js
@@ -10,8 +10,8 @@ const AWS = require('aws-sdk');
 const path = require('path');
 
 AWS.config.update({
-  accessKeyId: 'process.env.AWS_ACCESS_KEY_ID',
-  secretAccessKey: 'process.env.AWS_SECRET_ACCESS_KEY'
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
 var s3 = new AWS.S3();
