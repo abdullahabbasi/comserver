b429ce44d5e0a9f663dcdf906aaab56256e572a0
Abdullah Abbasi <abdullah.abbasi@oath.com>
2019-07-18 21:03:56 -0700

    ip address

--- routes.js
+++ routes.js
@@ -88,6 +88,11 @@ router.post('/photoUpload',upload.single('fileData'), (req, res,next) => {
 
 router.get('/getAllPosts', function(req, res){
   console.log('Request recieved get all products');
+  var ip = req.headers['x-forwarded-for'] || 
+  req.connection.remoteAddress || 
+  req.socket.remoteAddress ||
+  (req.connection.socket ? req.connection.socket.remoteAddress : null);
+  console.log('ip address calling from ', ip);
   var start = new Date();
   var sql = "select * from post;"
    var resultArray  = [];
